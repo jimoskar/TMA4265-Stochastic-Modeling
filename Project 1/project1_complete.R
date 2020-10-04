f22db7613957f4e25515b27e1c6b70aca4ed638d
alexaoh <alex.andunge@gmail.com>
2020-10-04 14:01:36 +0200

    Made my changes to final code.

--- Project 1/project1_complete.R
+++ Project 1/project1_complete.R
@@ -1,8 +1,6 @@
-
 # ==============================================
-# PROBLEM 1D
+# PROBLEM 1d)
 # ==============================================
-
 
 beta <-  0.05
 gamma <-  0.10
@@ -10,35 +8,33 @@ alpha  <-  0.01
 
 P  <-  matrix(c(1-beta, beta, 0, 0, 1 - gamma, gamma, alpha, 0, 1-alpha), nrow <- 3, ncol <- 3, byrow <- TRUE)
 
-
-# This function returns the mean over a specified number of simulations of the expected values
-simulate_expected_values <- function(P, numSim = 1) {
+# Return the mean over a specified number of simulations of the expected values.
+simulate_expected_values <- function(P, num_sim = 1) {
   
-  result <- matrix(nrow = numSim, ncol = 3)
+  result <- matrix(nrow = num_sim, ncol = 3)
   
-  for (j in 1:numSim) {
-  
-    v1 <- 0 #total number of "loops" in state 0
-    v2 <- 0 #total number of "loops" in state 1
-    v3 <- 0 #total number of transitions from 0 to 1 and 1 to 2
+  for (j in 1:num_sim) {
     
-    v1_counter <- 0 #number of transitions from 0 to 1
-    v2_counter <- 0 # number of transitions from 1 to 2
-    v3_counter <- 0 # number of complete cycles
+    v1 <- 0 # Total number of "loops" in state 0.
+    v2 <- 0 # Total number of "loops" in state 1.
+    v3 <- 0 # Total number of transitions from 0 to 1 and 1 to 2.
+    
+    v1_counter <- 0 # Number of transitions from 0 to 1.
+    v2_counter <- 0 # Number of transitions from 1 to 2.
+    v3_counter <- 0 # Number of complete cycles.
     
     year <- 365
     state <- 0
     for (i in 1:(year*50)) {
       
-      
       newstate <- sample.int(3, size <- 1, replace <- TRUE, prob <- P[state+1,])-1
       
-      if (newstate != state) { # a new state has been transistioned to 
+      if (newstate != state) { # A new state has been transitioned to.
         if(state == 0) {
           v1_counter <- v1_counter + 1
           v1 <- v1 + 1
           v2 <- v2 + 1
-          v3 <- v3 +1
+          v3 <- v3 + 1
         }
         else if(state == 1) {
           v2_counter <- v2_counter + 1
@@ -51,13 +47,13 @@ simulate_expected_values <- function(P, numSim = 1) {
         }
         
       }
-      else { #The transistion was to the same state
+      else { # Transistion to the same state.
         if (state == 0) {
-          v1 <- v1 +1
+          v1 <- v1 + 1
           v2 <-  v2 + 1
           v3 <- v3 + 1}
         else if (state == 1) {
-          v2 <- v2 +1
+          v2 <- v2 + 1
           v3 <- v3 + 1}
         else if (state == 2){
           v3 <- v3 + 1
@@ -70,84 +66,35 @@ simulate_expected_values <- function(P, numSim = 1) {
     
     result[j,] <- c(v1/v1_counter,v2/v2_counter,v3/v3_counter)
   }
-  return (c(mean(result[,1]),mean(result[,2]),mean(result[,3]))) #returns the mean over the simulations
+  return (c(mean(result[,1]),mean(result[,2]),mean(result[,3]))) # Return the means over the simulations.
 }
 
-# +++++++++++++++++++
-# Run code under here
-# +++++++++++++++++++
+# ++++++++++++++
+# Run code below
+# ++++++++++++++
 
-
-
-#simulate_expected_values(P,100)
-
+# Find the mean of the expected values of 100 realizations of the Markov chain simulated for 50 years. 
+result <- simulate_expected_values(P,100)
+cat("Theoretical value 1: ", 1/beta, "\n")
+cat("Simulated value 1: ", result[1], "\n")
+cat("Theoretical value 2: ", 1/beta + 1/gamma, "\n")
+cat("Simulated value 2: ", result[2], "\n")
+cat("Theoretical value 3: ", 1/beta + 1/alpha + 1/gamma, "\n")
+cat("Simulated value 3: ", result[3], "\n")
 
 
 # ==============================================
-# PROBLEM 1F
+# PROBLEM 1f)
 # ==============================================
-#Highlight the code underneath and run it to get the answers to f)
 
+# Probabilities in the transition probability matrix. 
+# alpha and gamma are already defined above. 
 
-
-N <- 1000 # Individuals.
-
-Y0 <- c(950, 50, 0) # Starting state.
-
-# Probabilities (to begin with).
-gamma <- 0.10
-alpha <- 0.01
-beta <- function(Y){
-  return (0.5*Y[2]/N)
-}
-n <- 300 # Time steps.
-
-values <- matrix(data=NA,nrow=3,ncol=n) # Preallocate matrix for simulated values. 
-values[, 1] <- Y0
-
-# Run simulation.
-for (t in 2:n){
-  # Use a binomial to simulate. 
-  old_susc <- values[1, t-1]
-  old_inf <- values[2, t-1]
-  old_rec <- values[3, t-1]
-  
-  new_inf <- rbinom(n = 1, size = old_susc, beta(values[, t-1]))
-  new_rec <- rbinom(n = 1, size = old_inf, gamma) 
-  new_susc <- rbinom(n = 1,size =  old_rec, alpha) 
-  Y <- c(old_susc - new_inf + new_susc, old_inf - new_rec + new_inf, old_rec - new_susc + new_rec)
-  values[, t] <- Y
+beta_n <- function(Y) {
+  return (0.5 * Y[2]/N)
 }
 
-par_lty <- c(3,2,1)
-par_col <- c("blue", "red", "green")
-plot(1:n, values[1, ], type = "l", lty = par_lty[1], col = par_col[1], xlab="Time [days]", ylab = "Individuals", main = "One Realization")
-lines(1:n, values[2, ], type = "l", lty = par_lty[2], col = par_col[2])
-lines(1:n, values[3, ], type = "l", lty = par_lty[3], col = par_col[3])
-legend("topright", legend= c("Susceptible", "Infected", "Recovered"), lty = par_lty, col = par_col)
-
-
-# ==============================================
-# PROBLEM 1G
-# ==============================================
-#Highlight the code underneath and run it to get the answers to g)
-
-N <- 1000 # Individuals.
-
-Y0 <- c(950, 50, 0) # Starting state.
-
-# Probabilities (to begin with).
-gamma <- 0.10
-alpha <- 0.01
-beta <- function(Y){
-  return (0.5*Y[2]/N)
-}
-n <- 36500 # Time steps. 100 years. 
-
-values <- matrix(data=NA,nrow=3,ncol=n) # Preallocate matrix for simulated values. 
-values[, 1] <- Y0
-
-sim <- function(values){
+sim <- function(values, n){
   # Run simulation.
   for (t in 2:n){
     # Use a binomial to simulate. 
@@ -155,16 +102,52 @@ sim <- function(values){
     old_inf <- values[2, t-1]
     old_rec <- values[3, t-1]
     
-    new_susc <- rbinom(n = 1, size = old_susc, 1-beta(values[, t-1]))
-    new_inf <- rbinom(n = 1, size = old_inf, 1-gamma) 
-    new_rec <- rbinom(n = 1,size =  old_rec, 1-alpha) 
-    Y <- c(new_susc - (new_rec - old_rec), new_inf - (new_susc - old_susc), new_rec - (new_inf - old_inf))
+    new_inf <- rbinom(n = 1, size = old_susc, beta_n(values[, t-1]))
+    new_rec <- rbinom(n = 1, size = old_inf, gamma) 
+    new_susc <- rbinom(n = 1,size =  old_rec, alpha) 
+    Y <- c(old_susc - new_inf + new_susc, old_inf - new_rec + new_inf, old_rec - new_susc + new_rec)
     values[, t] <- Y
   }
-  return (values)
+
+  # Foreshadow: For task h). 
+  m <- max(values[2, ]) # Find maximum value of the infected individuals at once. 
+  t <- which.max(values[2, ]) # Find index of the maximum of the infected individuals at once.
+  return (list("values" = values, "m" = m, "t" = t))
 }
 
-take.mean <- function(amount, fun = sim){
+plot_one_realization <- function(values, n, fun = sim){
+  list_of_values <- fun(values, n)
+  values <- list_of_values$values # Only need the values in this part of the task. 
+  par_lty <- c(3,2,1)
+  par_col <- c("blue", "red", "green")
+  plot(1:n, values[1, ], type = "l", lty = par_lty[1], col = par_col[1], xlab="Time [days]", ylab = "Individuals", main = "One Realization")
+  lines(1:n, values[2, ], type = "l", lty = par_lty[2], col = par_col[2])
+  lines(1:n, values[3, ], type = "l", lty = par_lty[3], col = par_col[3])
+  legend("topright", legend= c("Susceptible", "Infected", "Recovered"), lty = par_lty, col = par_col)
+}
+
+# ++++++++++++++
+# Run code below
+# ++++++++++++++
+
+# Initial distribution over the categories.
+Y0 <- c(950,50,0)
+N <- sum(Y0) # Number of individuals. 
+
+n <- 300 # Time steps.
+
+# Preallocate matrix for simulated values.
+values <- matrix(data=NA,nrow=3,ncol=n)  
+values[, 1] <- Y0
+
+# Plot figure showing one realization. 
+plot_one_realization(values, n, sim)
+
+# ==============================================
+# PROBLEM 1g)
+# ==============================================
+
+take.mean <- function(values, amount, n, fun = sim){
   # Take mean of 'amount' number of simulations. 
   average_susc <- c(length = amount)
   average_inf <- c(length = amount)
@@ -172,7 +155,8 @@ take.mean <- function(amount, fun = sim){
   
   for (i in 1:amount){
     # Take an average. 
-    val <- sim(values)
+    list_of_values <- sim(values, n)
+    val <- list_of_values$values # Only need the values in this part of the task. 
     susc <- val[1, ncol(val)]/N
     inf <- val[2, ncol(val)]/N
     rec <- val[3, ncol(val)]/N
@@ -183,72 +167,89 @@ take.mean <- function(amount, fun = sim){
   return (list("val" = val, "avg1" = mean(average_susc), "avg2" = mean(average_inf), "avg3" = mean(average_rec)))
 }
 
+plot_one_hundred_years <- function(val){
+  # Parameters for color and linetypes in plot. 
+  par_lty <- c(3,2,1)
+  par_col <- c("blue", "red", "green")
+  # Plot the lines. 
+  plot(1:n, val[1, ], type = "l", lty = par_lty[1], col = par_col[1], xlab="Time [days]", 
+      ylab = "Individuals", main = capture.output(cat("Mean over ", amount, " realizations")))
+  lines(1:n, val[2, ], type = "l", lty = par_lty[2], col = par_col[2])
+  lines(1:n, val[3, ], type = "l", lty = par_lty[3], col = par_col[3])
+  legend("topright", legend= c("Susceptible", "Infected", "Recovered"), lty = par_lty, col = par_col)
+}
+
+# ++++++++++++++
+# Run code below
+# ++++++++++++++
+
+n <- 36500 # Time steps. 100 years. 
+
+# Preallocate matrix for simulated values.
+values <- matrix(data=NA,nrow=3,ncol=n)  
+values[, 1] <- Y0
+
 amount <- 100 # Number of simulations to take mean over. 
-avg_prop <- take.mean(amount, sim)
+avg_prop <- take.mean(values, amount, n, sim)
 val <- avg_prop$val # Values to plot. 
 
-# Parameters for color and linetypes in plot. 
-par_lty <- c(3,2,1)
-par_col <- c("blue", "red", "green")
-
-# Plot the lines. 
-plot(1:n, val[1, ], type = "l", lty = par_lty[1], col = par_col[1], xlab="Time [days]", 
-     ylab = "Individuals", main = capture.output(cat("Mean over ", amount, " realizations")))
-lines(1:n, val[2, ], type = "l", lty = par_lty[2], col = par_col[2])
-lines(1:n, val[3, ], type = "l", lty = par_lty[3], col = par_col[3])
-legend("topright", legend= c("Susceptible", "Infected", "Recovered"), lty = par_lty, col = par_col)
+plot_one_hundred_years(val)
 
 # Print simulated values. 
 print("Mean Proportions:", quote=FALSE)
 cat("Susceptible ", avg_prop$avg1, "\n")
 cat("Infected ", avg_prop$avg2, "\n")
-cat("Recovered ", avg_prop$avg3)
+cat("Recovered ", avg_prop$avg3, "\n")
+
 
 # ==============================================
-# PROBLEM 1H
+# PROBLEM 1h)
 # ==============================================
 
-
-#Returns the mean over the maximum number of infected in a simulation and the corresponding time for a given number of simulations
-estimate_max <- function(Y0,iterations,simulations) {
-  maximum_sum = 0
-  maximum_time_sum = 0
-  for (i in 1:simulations){
-    Y = Y0
-    I_max = 0
-    n_max = 0
-    for (j in 1:iterations) {
-      if (Y[2] > I_max) {
-        I_max = Y[2]
-        n_max = j
-      }
-      Y = simulate_step(Y)
-    }
+# Find the expected values in question.  
+find.expected.values <- function(values, amount, n, fun = sim){
+  maximums <- c()
+  argmax <- c()
+  for (i in 1:amount){
+    vec <- sim(values, n)
+    val <- vec$values
     
-    maximum_sum = maximum_sum + I_max
-    maximum_time_sum = maximum_time_sum + n_max
+    # Using the maximum and argmax from sim() here. 
+    m <- vec$m
+    t <- vec$t
+    maximums[i] <- m
+    argmax[i] <- t
   }
-  return (c(maximum_sum/simulations,maximum_time_sum/simulations))
+
+  return (list("exp1" = mean(maximums), "exp2" = mean(t)))
 }
 
-Estimates <- estimate_max(Y0,300,1000)
-print(Estimates)
+# ++++++++++++++
+# Run code below
+# ++++++++++++++
 
+n <- 300 # Time steps.
+
+# Preallocate matrix for simulated values.
+values <- matrix(data=NA,nrow=3,ncol=n)  
+values[, 1] <- Y0
+
+means <- find.expected.values(values, N, n, sim)
+cat("Expected value of max of I's ", means$exp1, "\n")
+cat("Expected time at which the number of infected individuals first takes its highest values ", means$exp2, "\n")
 
 # ==============================================
-# PROBLEM 2A
+# PROBLEM 2a)
 # ==============================================
 
-lambda = 1.5
-
-#Simulates the poisson process for a given number of simulations
+# Simulate the Poisson process for a given number of simulations.
 simulate_pois <- function(lambda, t_start,t_end, plot, numSim = 1) {
-  result <-  rep(NA,numSim)
+  result <- rep(NA,numSim)
   for (i in 1:numSim) {
-    n = rpois(1,lambda = lambda*(t_end - t_start))
-    result[i] = n
-    v_vec = runif(n,t_start,t_end)
-    w_vec = sort(v_vec)
+    n <-  rpois(1,lambda = lambda*(t_end - t_start))
+    result[i] <-  n
+    v_vec <-  runif(n,t_start,t_end)
+    w_vec <-  sort(v_vec)
     x_vec <- seq(from = 0, to = n, by = 1)
     
     if(plot && i == 1) {
@@ -267,30 +268,25 @@ simulate_pois <- function(lambda, t_start,t_end, plot, numSim = 1) {
   return (result)
 }
 
-# +++++++++++++++++++
-# Run code under here
-# +++++++++++++++++++
+# ++++++++++++++
+# Run code below
+# ++++++++++++++
 
+lambda <- 1.5 # Poisson rate parameter. 
 
-#Estimate the probability in question
+# Estimate the probability in question.
 res <- simulate_pois(lambda,0,59,plot = FALSE, numSim = 1000)
 num <- sum(res > 100)/length(res)
-num
+cat("Probability: ", num, "\n")
 
-#plotting
+# Plot.
 simulate_pois(lambda,0,59,plot = TRUE, numSim = 10)
-
 
 # ==============================================
 # PROBLEM 2B
 # ==============================================
 
-
-res <- simulate_pois(lambda,0,59,plot = FALSE, numSim = 1000)
-mean(res)
-var(res)
-
-#Takes a list of poisson distributed values (claims) and returns the size of the claims summed together
+# Takes a list of Poisson distributed values (claims) and returns the size of the claims summed together
 calculate.claims <- function(claims) {
   result = rep(0,length(claims))
   for (i in 1:length(claims)) {
@@ -302,16 +298,12 @@ calculate.claims <- function(claims) {
   return(result)
 }
 
-
 # +++++++++++++++++++
 # Run code under here
 # +++++++++++++++++++
 
-
 claims <- simulate_pois(lambda,0,59,plot = FALSE, numSim = 1000*10)
 res <- calculate.claims(claims)
 
-mean(res)
-var(res)
-
-
+cat("Expected total claim amount: ", mean(res), "\n")
+cat("Variance of the total claim amount: ", var(res))

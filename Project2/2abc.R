eedb301eb98bf043f4e625d45582b6db6bfce2f2
Jim Totland <jimtotland@dhcp-10-24-1-38.wlan.ntnu.no>
2020-11-07 11:22:04 +0100

    small changes to files

--- Project2/2abc.R
+++ Project2/2abc.R
@@ -1,3 +1,5 @@
+#Project 2 Stochastic Modelling
+#Promblem 2: Calibrating Climate Models
 
 #2.a
 std = 0.5
@@ -8,12 +10,13 @@ cov_function <- function(theta1, theta2){
 }
 
 theta_grid = seq(from = 0.25, to = 0.50, by = 0.0049999)
-theta_grid
 
+#Known values
 sample_points =  c(0.30,0.35,0.39,0.41,0.45)
 sample_values = c(0.5, 0.32, 0.40, 0.35, 0.60)
 
 
+#Function for creating covariance matrix
 get_cov <-  function(vecA,vecB) {
   nA <- length(vecA)
   nB <- length(vecB)
@@ -27,7 +30,6 @@ get_cov <-  function(vecA,vecB) {
 }
 
 Sigma_AB <-  get_cov(theta_grid, sample_points)
-Sigma_AB
 Sigma_AA <-  get_cov(theta_grid, theta_grid)
 Sigma_BB <- get_cov(sample_theta, sample_points)
 
@@ -36,9 +38,7 @@ muA <-  rep(mu,length(theta_grid))
 muB <- rep(mu,length(sample_points))
 
 muC <- muA + Sigma_AB %*% solve(Sigma_BB) %*% (sample_values - muB)
-muC
 Sigma_C <- Sigma_AA - Sigma_AB %*% solve(Sigma_BB) %*% t(Sigma_AB)
-Sigma_C
 
 
 L = chol(Sigma_C)
@@ -58,25 +58,24 @@ lower = muC - z005*sqrt(diag(Sigma_C))
 lines(theta_grid,upper, col = "green")
 lines(theta_grid,lower, col = "red")
 
-#2.b
 
+
+#2.b
 library(expm)   
 y <- rep(0.30,length(theta_grid))
 std_matrix <- Sigma_C %^% -0.5
-muC
-standardize <- solve(std_matrix) %*% (y - muC)
-standardize
-probs <-  pnorm(standardize)
+standardize <- std_matrix %*% (y - muC)
+probs <- pnorm(standardize)
 plot(theta_grid, probs)
 lines(theta_grid, probs)
 
 #2.c
 
+#Same as in a), but with one more point.
 sample_points =  c(0.30,0.35,0.39,0.41,0.45, 0.33)
 sample_values = c(0.5, 0.32, 0.40, 0.35, 0.60, 0.40)
 
 Sigma_AB <-  get_cov(theta_grid, sample_points)
-Sigma_AB
 Sigma_AA <-  get_cov(theta_grid, theta_grid)
 Sigma_BB <- get_cov(sample_points, sample_points)
 
